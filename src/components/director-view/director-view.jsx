ed003abeb17a2662eede9db620ce5e6e9365ab9b
Monika <91907563+mkford4@users.noreply.github.com>
2022-04-05 18:32:27 -0500

    Updated director & genre views with Bootstrap & PropTypes

---
+++ src/components/director-view/director-view.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { Container, Card, Button, Row } from 'react-bootstrap/';
+
+export class DirectorView extends React.Component {
+
+  render() {
+    const { director, onBackClick, movies } = this.props;
+
+    return (
+      <Container fluid>
+        <Card>
+          <Card.Body>
+            <Card.Title>Director</Card.Title>
+            <Card.Text>
+              <span className="label">Name: </span>
+              <span className="value">{director.Name}</span>
+            </Card.Text>
+            <Card.Text>
+              <span className="label">Bio: </span>
+              <span className="value">{director.Bio}</span>
+            </Card.Text>
+            <Card.Text>
+              <span className="label">Birth Year: </span>
+              <span className="value">{director.Birth}</span>
+            </Card.Text>
+            <Card.Text>
+              <span className="label">Death: </span>
+              <span className="value">{director.Death}</span>
+            </Card.Text>
+
+            <Button variant="seconday" onClick={() => { onBackClick(); }}>
+              Back
+            </Button>
+          </Card.Body>
+        </Card>
+        <Row>
+          {movies.map(movie => (
+            <Card className="" key={movie._id} >
+              <Card.Img
+                className=""
+                variant="top"
+                src={movie.ImagePath} />
+              <Card.Body style={{ backgroundColor: "black" }}>
+                <Card.Title className="">
+                  {movie.Title}
+                </Card.Title>
+              </Card.Body>
+            </Card>
+          ))}
+        </Row>
+      </Container>
+    );
+  }
+}
+
+DirectorView.proptypes = {
+  director: PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+    Bio: PropTypes.string.isRequired,
+    Birth: PropTypes.string.isRequired,
+    Death: PropTypes.string
+  })
+};
---
+++ src/components/genre-view/genre-view.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { Container, Card, Button, Row } from 'react-bootstrap/';
+
+export class GenreView extends React.Component {
+
+  render() {
+    const { genre, onBackClick, movies } = this.props;
+
+    return (
+      <Container fluid>
+        <Card>
+          <Card.Body>
+            <Card.Title>Genre</Card.Title>
+            <Card.Text>
+              <span className="label">Name: </span>
+              <span className="value">{genre.Name}</span>
+            </Card.Text>
+            <Card.Text>
+              <span className="label">Description: </span>
+              <span className="value">{genre.Description}</span>
+            </Card.Text>
+
+            <Button variant="seconday" onClick={() => { onBackClick(); }}>
+              Back
+            </Button>
+          </Card.Body>
+        </Card>
+        <Row>
+          {movies.map(movie => (
+            <Card className="" key={movie._id} >
+              <Card.Img
+                className=""
+                variant="top"
+                src={movie.ImagePath} />
+              <Card.Body style={{ backgroundColor: "black" }}>
+                <Card.Title className="">
+                  {movie.Title}
+                </Card.Title>
+              </Card.Body>
+            </Card>
+          ))}
+        </Row>
+      </Container>
+    );
+  }
+}
+
+GenreView.proptypes = {
+  genre: PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired
+  }).isRequired,
+};

7ddaee1046833afd60899e17c14ec116329574de
Monika <91907563+mkford4@users.noreply.github.com>
2022-05-12 14:34:48 -0500

    Created reducers.js and actions.js, and created Store in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import MainView root component
-import { MainView } from './components/main-view/main-view';
 import { Container } from 'react-bootstrap/';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+import moviesApp from './reducers/reducers';
 
-//import to bundle scss file with jsx
+import MainView from './components/main-view/main-view';
 import './index.scss';
 
+const store = createStore(moviesApp, devToolsEnhancer());
 //Main component
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <Container>
-        <MainView />
-      </Container>
+      <Provider store={store}>
+        <Container>
+          <MainView />
+        </Container>
+      </Provider>
     );
   }
 }
